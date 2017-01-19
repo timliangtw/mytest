ce1baadb6ed5a48d7c135b3b29e0c5ae959a44df
timliangtw <tim.liang@advantech.com.tw>
2017-01-19 14:12:08 +0800

    m

--- test.sh
+++ test.sh
@@ -1,1 +1,9 @@
-echo "-----220------$U220A1_SOLO"
+#!/bin/sh
+git clone https://github.com/timliangtw/mytest.git
+cd mytest
+touch 1234
+git add 1234
+git commit -m '$Release_Note'
+git push
+git tag -a jenkins -m '$Release_Note'
+git push origin jenkins
